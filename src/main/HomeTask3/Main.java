c624da5d5ecba4ef0f44b15f4be274b0df9a55aa
anemchenko <anemchenko@nes.ru>
2020-10-04 15:40:04 +0300

    Hometask3 init

---
+++ src/main/HomeTask3/Main.java
@@ -0,0 +1,18 @@
+package HomeTask3;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer i1 = new Integer(10);
+        Integer i2 = 11;
+        Integer i3 = new Integer(i2 - 1);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i1 == i3);
+        System.out.println(i1.equals(i3));
+    try{
+
+    }
+    catch()
+    }
+}
